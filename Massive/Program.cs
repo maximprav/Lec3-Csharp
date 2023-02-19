d6af38a11b9478b9692d739dc5a98e3e67998283
Maxim Prav <mxmlknv@mail.ru>
2023-02-19 14:01:17 +0300

    Сортировка Массива

---
+++ Massive/Program.cs
@@ -0,0 +1,34 @@
+﻿// Сортировка массива
+Console.Clear();
+int[] arr = { 1, 5, 2, 3, 4, 7, 6, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i=0; i<count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SortArray(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SortArray(arr);
+PrintArray(arr);
