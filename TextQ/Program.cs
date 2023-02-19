cff43bff0a194edc4f5a1cdbd29d3c790204f013
Maxim Prav <mxmlknv@mail.ru>
2023-02-19 13:38:03 +0300

    Работа с текстом

---
+++ TextQ/Program.cs
@@ -0,0 +1,26 @@
+﻿// Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить черточками,
+// маленькие буквы "к" заменить на заглавные, 
+// а заглавные "С" заменить на прописные.
+
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что";
+
+// string s = "qwerty"
+//             012345
+// s[3] // r
+//                        конкретный символ CHAR
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
